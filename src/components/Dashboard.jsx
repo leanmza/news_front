76c166fc734d9d2170c3667c26f4cffa9a816636
Leandro Hidalgo <boshahidalgo@hotmail.com>
2024-05-21 15:35:21 -0300

    ajustes menores

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,24 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 import { useParams } from "react-router-dom";
-import HorizontalCard from "./HorizontalCard";
-import "../assets/Dashboard.css"
+import "../assets/Dashboard.css";
 import BannerMain from "./banners/BannerMain";
 
 const Dashboard = ({ lastPublications, publicaciones }) => {
   const { category } = useParams();
   const { query } = useParams();
+  const [emptyResult, setEmptyResult] = useState(false);
 
   const className = {
     gridPublications: "col-12 col-sm-6 col-md-4 colCard",
     lastPublications: "col-12 col-sm-6 ultimas",
   };
-
-  const classNameImg = {
-    gridCard: "cardImg",
-    lastPublicationCard: "lastPublicationCard cardImg"
-  }
-  let emptyResult = false;
 
   if (category !== null && category !== undefined) {
     // Filtrar las publicaciones por categoría si se proporciona una categoría válida
@@ -33,7 +27,7 @@ const Dashboard = ({ lastPublications, publicaciones }) => {
     );
 
     if (publicaciones.length === 0) {
-      emptyResult = true;
+      setEmptyResult(true);
     }
   }
 
@@ -45,19 +39,27 @@ const Dashboard = ({ lastPublications, publicaciones }) => {
         </h5>
       ) : null}
 
-  {lastPublications ? (
-      <div className="lastPublications row col-10">
-        {lastPublications.map((item) => (
-          <Card key={item.id} item={item} className={className.lastPublications} classNameImg={classNameImg.lastPublicationCard}></Card>
-        ))}
-      </div>
-       ) : null}
+      {lastPublications ? (
+        <div className="lastPublications row col-10">
+          {lastPublications.map((item) => (
+            <Card
+              key={item.id}
+              item={item}
+              className={className.lastPublications}
+            ></Card>
+          ))}
+        </div>
+      ) : null}
       {/* <HorizontalCard lastPublications={lastPublications} /> */}
 
       <BannerMain></BannerMain>
       <div className="cardsMain row">
         {publicaciones.map((item) => (
-          <Card key={item.id} item={item} className={className.gridPublications} classNameImg={classNameImg.gridCard}></Card>
+          <Card
+            key={item.id}
+            item={item}
+            className={className.gridPublications}
+          ></Card>
         ))}
       </div>
     </div>
